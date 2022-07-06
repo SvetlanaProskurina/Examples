6ed0f035a0636effadd6aa51350c301f03cd1c53
Svetlana Proskurina <amourka@yandex.ru>
2022-07-07 00:34:36 +0300

    add sum double random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
-int result = numberA + numberB;
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine("numberA = "+numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine("numberB = "+numberB);
+double result = numberA + numberB;
 Console.WriteLine(result);
